68f126d9b97b304e0947faf73442af6b5b10b131
Ashlesh Pandey <ashlesh@shrigsolutions.com>
2025-06-03 10:50:07 +0545

    fix: dynamic toolbar height

--- lib/src/widgets/media_picker_app_bar.dart
+++ lib/src/widgets/media_picker_app_bar.dart
@@ -58,96 +58,101 @@ class _MediaAppBarState extends State<MediaAppBar> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      child: AnimatedContainer(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-        height: _isExpanded
-            ? MediaQuery.of(context).size.height
-            : kToolbarHeight - 8,
-        decoration:
-            BoxDecoration(color: widget.albumDropdownColor ?? Colors.white),
+      child: Container(
+        decoration: BoxDecoration(
+          color: widget.albumDropdownColor ?? Colors.white,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                      if (widget.onClose != null) {
-                        widget.onClose!();
-                      }
-                    },
-                    child: widget.closeIcon ??
-                        Icon(
-                          Icons.close,
-                          color: widget.closeIconColor,
-                        ),
-                  ),
-                  GestureDetector(onTap: () {
-                    if (_mediaAlbum.isEmpty) return;
-                    setState(() {
-                      _isExpanded = !_isExpanded;
-                    });
-                  }, child: SizedBox(
-                    child: BlocBuilder<MediaPickerCubit, MediaPickerState>(
-                      builder: (context, state) {
-                        final name = _selected ??
-                            ((widget.customAlbum != null &&
-                                    !widget.customAlbum!.isEmpty())
-                                ? (widget.customAlbum?.name ?? state.media.name)
-                                : state.media.name);
-                        final isEnabled =
-                            state.isLoading && _mediaAlbum.isEmpty;
-                        return widget.albumButtonBuilder != null
-                            ? widget.albumButtonBuilder!(
-                                isEnabled, name, _isExpanded)
-                            : DefaultAlbumButton(
-                                dropdownButtonColor: widget.dropdownButtonColor,
-                                isEnabled: isEnabled,
-                                name: name,
-                                isExpanded: _isExpanded,
-                              );
-                      },
-                    ),
-                  )),
-                  const SizedBox.shrink(),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: _mediaAlbum.length,
-                itemBuilder: (context, index) => GestureDetector(
-                  behavior: HitTestBehavior.translucent,
+            /// Fixed Row with close icon and toggle
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
                   onTap: () {
-                    setState(() {
-                      _selected = _mediaAlbum[index].name;
-                      widget.onChanged(MediaAlbum(
-                          id: _mediaAlbum[index].id,
-                          name: _selected ?? '',
-                          size: _mediaAlbum[index].size));
-                      _isExpanded = false;
-                    });
+                    Navigator.of(context).pop();
+                    if (widget.onClose != null) {
+                      widget.onClose!();
+                    }
                   },
-                  child: widget.albumTile != null
-                      ? Builder(
-                          builder: (context) {
-                            return widget.albumTile!(
-                                context, _mediaAlbum[index]);
-                          },
-                        )
-                      : DefaultAlbumTile(
-                          mediaAlbum: _mediaAlbum[index],
-                          albumNameStyle: widget.albumNameStyle,
-                          albumCountStyle: widget.albumCountStyle,
-                        ),
+                  child: widget.closeIcon ??
+                      Icon(
+                        Icons.close,
+                        color: widget.closeIconColor,
+                      ),
                 ),
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 10,
+                GestureDetector(onTap: () {
+                  if (_mediaAlbum.isEmpty) return;
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                }, child: SizedBox(
+                  child: BlocBuilder<MediaPickerCubit, MediaPickerState>(
+                    builder: (context, state) {
+                      final name = _selected ??
+                          ((widget.customAlbum != null &&
+                                  !widget.customAlbum!.isEmpty())
+                              ? (widget.customAlbum?.name ?? state.media.name)
+                              : state.media.name);
+                      final isEnabled = state.isLoading && _mediaAlbum.isEmpty;
+                      return widget.albumButtonBuilder != null
+                          ? widget.albumButtonBuilder!(
+                              isEnabled, name, _isExpanded)
+                          : DefaultAlbumButton(
+                              dropdownButtonColor: widget.dropdownButtonColor,
+                              isEnabled: isEnabled,
+                              name: name,
+                              isExpanded: _isExpanded,
+                            );
+                    },
+                  ),
+                )),
+                const SizedBox.shrink(),
+              ],
+            ),
+
+            /// Animated dropdown section
+            Flexible(
+              child: AnimatedSize(
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeInOut,
+                child: Offstage(
+                  offstage: _isExpanded,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: ListView.separated(
+                      itemCount: _mediaAlbum.length,
+                      itemBuilder: (context, index) => GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          setState(() {
+                            _selected = _mediaAlbum[index].name;
+                            widget.onChanged(MediaAlbum(
+                              id: _mediaAlbum[index].id,
+                              name: _selected ?? '',
+                              size: _mediaAlbum[index].size,
+                            ));
+                            _isExpanded = false;
+                          });
+                        },
+                        child: widget.albumTile != null
+                            ? Builder(
+                                builder: (context) {
+                                  return widget.albumTile!(
+                                      context, _mediaAlbum[index]);
+                                },
+                              )
+                            : DefaultAlbumTile(
+                                mediaAlbum: _mediaAlbum[index],
+                                albumNameStyle: widget.albumNameStyle,
+                                albumCountStyle: widget.albumCountStyle,
+                              ),
+                      ),
+                      separatorBuilder: (_, __) => const SizedBox(height: 10),
+                    ),
+                  ),
                 ),
               ),
             ),
