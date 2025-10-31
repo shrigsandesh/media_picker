df3b9157f113cf2ac399cde09113e531f49101d4
Ashlesh Pandey <ashlesh@shrigsolutions.com>
2025-10-31 14:12:30 +0545

    fix: return tab builder

--- lib/src/widgets/media_picker_app_bar.dart
+++ lib/src/widgets/media_picker_app_bar.dart
@@ -149,7 +149,7 @@ class _MediaAppBarState extends State<MediaAppBar>
 
           // Use custom builder if provided
           if (widget.tabBuilder != null) {
-            widget.tabBuilder!(context, album, isSelected);
+            return widget.tabBuilder!(context, album, isSelected);
           }
 
           // Default tab
